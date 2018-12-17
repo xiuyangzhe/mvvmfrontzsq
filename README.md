08fceb5cf0538ebb6507f23119346cf3addb77c6
Rocky Zhou <xiuyangzhe@live.com>
2018-12-17 16:49:13 +0800

    sample server

--- README.md
+++ README.md
@@ -1,3 +1,8 @@
 # mvvmfrontzsq
 前端框架，mvvm结构 zsqfront  
 ts语言
+##开始步骤：
+#1.确保环境node、typescript -g、supervisor -g
+#2.npm install
+#3.dev
+
--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,19 @@
-interface ICache{
-    useCache:boolean;
-    [propName:string]:any;
-}
-const cache:ICache = {useCache:true};
+// interface ICache{
+//     useCache:boolean;
+//     [propName:string]:any;
+// }
+// const cache:ICache = {useCache:true};
+import * as http from 'http';
+
+const server = http.createServer((req,res)=>
+{
+
+    res.writeHead(200,{'Content-Type':'text/plain'});
+    res.write("Hello World ts");
+    res.end();
+});
+
+server.listen(3000,()=>{
+    console.log("create a server port 3000");
+});
+
